e1766ce442fd9548e38f2813abc55d5ead751edf
Francesco Tuveri <francesco.tuveri@kuldat.com>
2015-10-06 12:45:17 +0200

    first commit

---
+++ GMM.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
+
+
+group_a = np.random.normal(loc=(20.00, 14.00), scale=(4.0, 4.0), size=(500, 2))
+group_b = np.random.normal(loc=(15.00, 8.00), scale=(2.0, 2.0), size=(500, 2))
+
+# plt.plot(group_a[:, 0], group_a[:, 1], 'ro')
+# plt.plot(group_b[:, 0], group_b[:, 1], 'bo')
+# plt.show()
+
+data = np.concatenate((group_a, group_b))
+mean_ext = np.mean(data, axis=0)
+std_ext = np.std(data, axis=0)
+# print std_ext
+# plt.plot(data[:, 0], data[:, 1], 'ro')
+# plt.plot(mean_ext[0], mean_ext[1], 'bo')
+
+distr = []
+distr.append({'weight': 0.5, 'mean': (0, 0), 'var': (0, 0), 'xi': 0, 'lam': 1, 'sig': 0, 'ni': 0})
+distr.append({'weight': 0.5, 'mean': (0, 0), 'var': (0, 0), 'xi': 0, 'lam': 1, 'sig': 0, 'ni': 0})
+
+# split centroid
+distr[0]['mean'] = mean_ext + np.multiply(std_ext, 0.1)
+distr[1]['mean'] = mean_ext - np.multiply(std_ext, 0.1)
+# distr[0]['var'] = (4.0, 4.0)
+# distr[1]['var'] = (2.0, 2.0)
+distr[0]['var'] = std_ext**2
+distr[1]['var'] = std_ext**2
+distr[0]['ni'] = distr[0]['weight'] * len(data)
+distr[1]['ni'] = distr[1]['weight'] * len(data)
+# plt.plot(distr[0]['mean'][0], distr[0]['mean'][1], 'go')
+# plt.plot(distr[1]['mean'][0], distr[1]['mean'][1], 'go')
+# plt.show()
+
+print ' distr 1'
+print distr[0]['mean']
+print distr[0]['var']
+
+print ' distr 2'
+print distr[1]['mean']
+print distr[1]['var']
+eval_train = []
+
+for iter in range(20):
+    print iter
+
+    gamma_denoms = []
+    for x in data:
+        gamma_denom = 0
+        for d in distr:
+            gamma_denom += np.multiply(d['weight'], np.prod(scipy.stats.norm(d['mean'], np.sqrt(d['var'])).pdf(x)))
+        gamma_denoms.append(gamma_denom)
+    sum_lik = sum(np.log(gamma_denoms))
+    eval_train.append(sum_lik)
+
+    gammas = []
+    for x, i in zip(data, range(len(data))):
+        num = np.multiply(distr[0]['weight'], np.prod(scipy.stats.norm(distr[0]['mean'], np.sqrt(distr[0]['var'])).pdf(x)))
+        gamma = np.divide(num, gamma_denoms[i])
+        gammas.append(gamma)
+    distr[0]['xi'] = sum(gammas)
+    mi_xk = np.dot(gammas, data)
+    mi_xk = np.divide(mi_xk, distr[0]['xi'])
+    new_mean_1 = np.multiply((1-distr[0]['lam']), distr[0]['mean']) + np.multiply(distr[0]['lam'], mi_xk)
+    sigma_xk = 0
+    for i in range(len(data)):
+        sigma_xk += np.multiply(gammas[i], np.multiply((data[i]-mi_xk), (data[i]-mi_xk)))
+    sigma_xk = np.divide(sigma_xk, distr[0]['xi'])
+    new_var_1 = np.multiply(distr[0]['lam'], sigma_xk)
+
+    gammas = []
+    for x, i in zip(data, range(len(data))):
+        num = np.multiply(distr[1]['weight'], np.prod(scipy.stats.norm(distr[1]['mean'], np.sqrt(distr[1]['var'])).pdf(x)))
+        gamma = np.divide(num, gamma_denoms[i])
+        gammas.append(gamma)
+    distr[1]['xi'] = sum(gammas)
+    mi_xk = np.dot(gammas, data)
+    mi_xk = np.divide(mi_xk, distr[1]['xi'])
+    new_mean_2 = np.multiply((1-distr[1]['lam']), distr[1]['mean']) + np.multiply(distr[1]['lam'], mi_xk)
+    sigma_xk = 0
+    for i in range(len(data)):
+        sigma_xk += np.multiply(gammas[i], np.multiply((data[i]-mi_xk), (data[i]-mi_xk)))
+    sigma_xk = np.divide(sigma_xk, distr[1]['xi'])
+    new_var_2 = np.multiply(distr[1]['lam'], sigma_xk)
+
+    denom_weight = 0
+    for d in distr:
+        denom_weight += d['xi']
+
+    new_weight_1 = np.divide((distr[0]['xi']), denom_weight)
+    new_weight_2 = np.divide((distr[1]['xi']), denom_weight)
+
+    distr[0]['mean'] = new_mean_1
+    distr[0]['var'] = new_var_1
+    distr[0]['weight'] = new_weight_1
+    distr[1]['mean'] = new_mean_2
+    distr[1]['var'] = new_var_2
+    distr[1]['weight'] = new_weight_2
+    print new_weight_1, new_weight_2
+
+
+print distr[0]['mean']
+print np.sqrt(distr[0]['var'])
+print distr[1]['mean']
+print np.sqrt(distr[1]['var'])
+
+plt.plot(eval_train)
+plt.show()
---
+++ utils.py
@@ -0,0 +1,52 @@
+import numpy as np
+import scipy
+
+def plot_cov_ellipse(cov, pos, volume=.5, ax=None, fc='none', ec=[0,0,0], a=1, lw=2):
+    """
+    Plots an ellipse enclosing *volume* based on the specified covariance
+    matrix (*cov*) and location (*pos*). Additional keyword arguments are passed on to the
+    ellipse patch artist.
+
+    Parameters
+    ----------
+        cov : The 2x2 covariance matrix to base the ellipse on
+        pos : The location of the center of the ellipse. Expects a 2-element
+            sequence of [x0, y0].
+        volume : The volume inside the ellipse; defaults to 0.5
+        ax : The axis that the ellipse will be plotted on. Defaults to the
+            current axis.
+    """
+
+    import numpy as np
+    from scipy.stats import chi2
+    import matplotlib.pyplot as plt
+    from matplotlib.patches import Ellipse
+
+    def eigsorted(cov):
+        vals, vecs = np.linalg.eigh(cov)
+        order = vals.argsort()[::-1]
+        return vals[order], vecs[:,order]
+
+    if ax is None:
+        ax = plt.gca()
+
+    vals, vecs = eigsorted(cov)
+    theta = np.degrees(np.arctan2(*vecs[:,0][::-1]))
+
+    kwrg = {'facecolor':fc, 'edgecolor':ec, 'alpha':a, 'linewidth':lw}
+
+    # Width and height are "full" widths, not radius
+    width, height = 2 * np.sqrt(chi2.ppf(volume,2)) * np.sqrt(vals)
+    ellip = Ellipse(xy=pos, width=width, height=height, angle=theta, **kwrg)
+
+    ax.add_artist(ellip)
+    plt.show()
+
+
+def compute_weighted_pdf(x, m, cov, w):
+    prec = np.linalg.inv(cov)
+    sign, logdet = np.linalg.slogdet(prec)
+    log_p_x_c = logdet/2. - np.dot(np.dot((x - m), prec), (x - m))/2.
+    log_w = np.log(w)
+    log_p_x = scipy.misc.logsumexp(np.array([log_p_x_c, log_w]))
+    return log_p_x
